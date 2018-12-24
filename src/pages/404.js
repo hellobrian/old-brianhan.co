eed7863c3f1295c25c7c8381bf3d2cc1f854c370
hellobrian <brianhan87usa@gmail.com>
2018-12-23 18:59:36 -0600

    chore: delete unused pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,10 +3,8 @@ import Layout from '../components/Layout';
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+    <h1>NOT FOUND</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 );
 
