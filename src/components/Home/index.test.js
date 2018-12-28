6459b21b81a24e378bdf543882cbbe43574a9b03
hellobrian <brianhan87usa@gmail.com>
2018-12-27 20:43:55 -0600

    test(Name): more tests

--- src/components/Home/index.test.js
+++ src/components/Home/index.test.js
@@ -3,8 +3,10 @@ import { render } from 'react-testing-library';
 import { Name, text } from './index';
 
 describe(`Name`, () => {
-  it(`renders name`, () => {
+  it(`renders name, description and mission`, () => {
     const { getByText } = render(<Name />);
     expect(getByText(text.name)).toBeInTheDocument();
+    expect(getByText(text.description)).toBeInTheDocument();
+    expect(getByText(text.mission)).toBeInTheDocument();
   });
 });
