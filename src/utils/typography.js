d34264d0a4fcc12c3be7859fe631b094a6b11995
hellobrian <brianhan87usa@gmail.com>
2020-02-05 14:15:14 -0600

    chore: update typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,23 +1,17 @@
-import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
+import Typography from 'typography';
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
-    },
-  }
-}
+const baseFontSize = 18;
 
-delete Wordpress2016.googleFonts
+const typography = new Typography({
+  baseFontSize: `${baseFontSize}px`,
+  baseLineHeight: 1.666,
+  headerFontFamily: ['Karla', 'Arial', 'sans-serif'],
+  bodyFontFamily: ['Merriweather', 'serif'],
+  scaleRatio: 2,
+});
 
-const typography = new Typography(Wordpress2016)
+export const rem = (px) => `${px / baseFontSize}rem`;
 
-// Hot reload typography in development.
-if (process.env.NODE_ENV !== `production`) {
-  typography.injectStyles()
-}
-
-export default typography
-export const rhythm = typography.rhythm
-export const scale = typography.scale
+export default typography;
+export const rhythm = typography.rhythm;
+export const scale = typography.scale;
