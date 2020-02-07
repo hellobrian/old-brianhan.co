8c10f606a73d5e703dcee5209bf6c7773b47dddf
hellobrian <brianhan87usa@gmail.com>
2020-02-07 10:10:56 -0600

    refactor(pages/index): create Post component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,50 +1,58 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
-
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { rhythm } from "../utils/typography"
+import React from 'react';
+import { graphql } from 'gatsby';
+import Bio from '../components/bio';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import { FancyLink } from '../components/FancyLink';
+import { rhythm } from '../utils/typography';
 
 const BlogIndex = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
-  const posts = data.allMarkdownRemark.edges
+  const siteTitle = data.site.siteMetadata.title;
+  const posts = data.allMarkdownRemark.edges;
 
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
       <Bio />
       {posts.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug
+        const title = node.frontmatter.title || node.fields.slug;
+        const slug = node.fields.slug;
+        const date = node.frontmatter.date;
+
         return (
-          <article key={node.fields.slug}>
-            <header>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-            </header>
-            <section>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </section>
-          </article>
-        )
+          <Post key={node.fields.slug} title={title} slug={slug} date={date}>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: node.frontmatter.description || node.excerpt,
+              }}
+            />
+          </Post>
+        );
       })}
     </Layout>
-  )
-}
+  );
+};
 
-export default BlogIndex
+const Post = ({ title, children, slug, date }) => (
+  <article>
+    <header>
+      <h3
+        style={{
+          marginBottom: rhythm(1 / 4),
+        }}>
+        <FancyLink style={{ boxShadow: `none` }} to={slug}>
+          {title}
+        </FancyLink>
+      </h3>
+      <small>{date}</small>
+    </header>
+    <section>{children}</section>
+  </article>
+);
+
+BlogIndex.Post = Post;
+
+export default BlogIndex;
 
 export const pageQuery = graphql`
   query {
@@ -69,4 +77,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
