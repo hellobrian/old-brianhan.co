69ebf8ad33f8643acdf0d6e5a445c9018c781e64
hellobrian <brianhan87usa@gmail.com>
2020-04-08 15:18:30 -0500

    feat(SvgCircle): add to components

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ export * from './Bio/Bio';
 export * from './FancyLink/FancyLink';
 export * from './Pagination/Pagination';
 export * from './Post/Post';
+export * from './SvgCircle/SvgCircle';
 
 // Used in every page and template
 export * from './Layout/Layout';
