14b71e4e7c606ec0af42acd68f746e2ee99840b2
hellobrian <brianhan87usa@gmail.com>
2020-05-16 12:54:00 -0500

    fix: make emojis accessible

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SvgCircle, FancyLink } from '../index';
-import { rhythm } from '../../utils/typography';
+import { rhythm, scale } from '../../utils/typography';
 
 // css
 import './_variables.css';
@@ -48,9 +48,51 @@ const Header = ({ pathname, title }) => {
             paddingRight: rhythm(1),
             paddingBottom: rhythm(1 / 2),
           }}>
-          <div>{title}</div>
-          <div>is a front-end developer.</div>
-          <div>Building and writing things for people on the internet.</div>
+          <details>
+            <summary>{title}</summary>
+            <ul
+              style={{
+                ...scale(0.5),
+                margin: 0,
+                marginLeft: '24px',
+                padding: rhythm(1),
+                listStyleType: 'none',
+                display: 'grid',
+                gridGap: '8px',
+              }}>
+              <li>
+                <span aria-label="job" role="img">
+                  ⚛️
+                </span>{' '}
+                <span id="job-description">UX developer</span>
+              </li>
+              <li>
+                <span aria-label="birthplace" role="img">
+                  🇨🇦
+                </span>{' '}
+                Born in Toronto
+              </li>
+              <li>
+                <span aria-label="residence" role="img">
+                  >🌮
+                </span>{' '}
+                Living and working in Austin
+              </li>
+              <li>
+                <span aria-label="what is this blog for?" role="img">
+                  🤔
+                </span>{' '}
+                This is where I write tutorials for myself and gush about stuff
+                that I love
+              </li>
+              <li>
+                <span aria-label="Brian loves you very much" role="img">
+                  ♥️
+                </span>{' '}
+                Thanks for stopping by!
+              </li>
+            </ul>
+          </details>
         </h1>
       ) : (
         <h3>
