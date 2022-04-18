a646aa57a46ac04e5edefdbe66ec92da91584c30
hellobrian <brianhan@hey.com>
2022-04-18 12:33:07 -0500

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()],
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-themes
+      theme: 'dracula',
+      // Manually specify langs
+      // Note: Shiki has countless langs built-in, including .astro!
+      langs: ['astro'],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
+  },
+});
