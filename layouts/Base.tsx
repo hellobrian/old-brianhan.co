e8906979b93b5eef222b8059878e0970e4c2f2a9
hellobrian <brianhan@hey.com>
2022-04-28 00:26:56 -0500

    Add MenuButton to base layout

--- layouts/Base.tsx
+++ layouts/Base.tsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
 import BaseHead from "components/BaseHead";
+import MenuIcon from "components/Icons/MenuIcon";
+import CloseIcon from "components/Icons/CloseIcon";
+
+function MenuButton() {
+  const [isOpen, setOpen] = useState(false);
+  const toggle = () => setOpen(!isOpen);
+  return (
+    <button type="button" onClick={toggle}>
+      {isOpen ? <CloseIcon /> : <MenuIcon />}
+    </button>
+  );
+}
 
 export default function Base({ children }) {
   return (
     <>
+      <MenuButton />
       <BaseHead />
       <main>{children}</main>
     </>
