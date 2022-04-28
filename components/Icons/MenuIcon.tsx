c64207021a3788c64559ad2ef7653e54cf62ed48
hellobrian <brianhan@hey.com>
2022-04-28 00:26:34 -0500

    add close and menu icons

---
+++ components/Icons/MenuIcon.tsx
@@ -0,0 +1,16 @@
+export default function MenuIcon({ width = 24, height = 24 }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+      width={width}
+      height={height}
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M4 8h16M4 16h16" />
+    </svg>
+  );
+}
