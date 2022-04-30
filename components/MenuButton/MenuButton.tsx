33b4e1ccb326bfbb968627ef23ff64e57b96878d
hellobrian <brianhan@hey.com>
2022-04-30 14:28:33 -0500

    remove useDelayedRender

--- components/MenuButton/MenuButton.tsx
+++ components/MenuButton/MenuButton.tsx
@@ -1,6 +1,3 @@
-// packages
-import useDelayedRender from "use-delayed-render";
-
 // components
 import MenuIcon from "components/Icons/MenuIcon";
 import CloseIcon from "components/Icons/CloseIcon";
@@ -13,20 +10,9 @@ import styles from "./MenuButton.module.scss";
 export default function MenuButton({ active }) {
   const { isOpen, open, close, toggle } = useToggle();
 
-  const { mounted, rendered } = useDelayedRender(isOpen, {
-    enterDelay: 200,
-    exitDelay: 200,
-  });
-
-  console.log({ active, mounted, rendered });
-
   return (
     <button type="button" onClick={toggle}>
-      {isOpen ? (
-        <CloseIcon className={rendered ? styles.fadeIn : styles.fadeOut} />
-      ) : (
-        <MenuIcon className={rendered ? styles.fadeIn : styles.fadeOut} />
-      )}
+      {isOpen ? <CloseIcon /> : <MenuIcon />}
     </button>
   );
 }
