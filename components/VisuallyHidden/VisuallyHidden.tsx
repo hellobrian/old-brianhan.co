3115bdf0f8c6e0bd5d8f7b3709fb1f765fb822cf
hellobrian <brianhan@hey.com>
2022-04-30 15:57:48 -0500

    move styles for VisuallyHidden into component file

--- components/VisuallyHidden/VisuallyHidden.tsx
+++ components/VisuallyHidden/VisuallyHidden.tsx
@@ -1,5 +1,21 @@
 import styles from "./VisuallyHidden.module.scss";
 
 export default function VisuallyHidden({ as: As, children }) {
-  return <As className={styles.VisuallyHidden}>{children}</As>;
+  return (
+    <>
+      <As className="visually-hidden">{children}</As>
+
+      <style jsx>{`
+        .visually-hidden:not(:focus):not(:active) {
+          clip: rect(0 0 0 0);
+          clip-path: inset(50%);
+          height: 1px;
+          overflow: hidden;
+          position: absolute;
+          white-space: nowrap;
+          width: 1px;
+        }
+      `}</style>
+    </>
+  );
 }
