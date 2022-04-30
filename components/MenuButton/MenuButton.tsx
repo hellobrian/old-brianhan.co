9d345b037c0ccf7a5f400bec314092466adbb365
hellobrian <brianhan@hey.com>
2022-04-30 15:57:21 -0500

    use framer-motion for MenuButton

--- components/MenuButton/MenuButton.tsx
+++ components/MenuButton/MenuButton.tsx
@@ -1,22 +1,29 @@
 // packages
-import { motion } from "framer-motion";
+import { motion, HTMLMotionProps } from "framer-motion";
 
 // components
 import MenuIcon from "components/Icons/MenuIcon";
 import CloseIcon from "components/Icons/CloseIcon";
 
-// hooks
-import useToggle from "hooks/useToggle/useToggle";
+// https://www.carlrippon.com/Different-ways-to-strongly-type-function-component-props-with-typescript/
+// https://github.com/framer/motion/issues/665#issuecomment-783401005
+type MenuButtonProps = HTMLMotionProps<"button"> & {
+  isOpen?: boolean;
+  onClick?: () => void;
+};
 
-export default function MenuButton() {
-  const { isOpen, open, close, toggle } = useToggle();
-
+export default function MenuButton({
+  isOpen = false,
+  onClick = () => {},
+  ...props
+}: MenuButtonProps) {
   return (
     <motion.button
       type="button"
-      onClick={toggle}
+      onClick={onClick}
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.9 }}
+      {...props}
     >
       {isOpen ? <CloseIcon /> : <MenuIcon />}
     </motion.button>
