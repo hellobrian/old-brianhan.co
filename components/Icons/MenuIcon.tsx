e5d9f2cc15ed705241b69a7cb9ffb71bcb272c09
hellobrian <brianhan@hey.com>
2022-04-30 18:02:41 -0500

    remove fill from icons

--- components/Icons/MenuIcon.tsx
+++ components/Icons/MenuIcon.tsx
@@ -3,7 +3,6 @@ export default function MenuIcon({ width = 24, height = 24, ...props }) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       className="h-6 w-6"
-      fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
       strokeWidth={2}
