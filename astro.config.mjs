ea1a3f3d25f61ecbf117905e3bd059d8a12d4d5d
Brian Han <hellobrian@users.noreply.github.com>
2023-01-19 16:44:36 -0600

    Astro again... (#60)
    * delete everything
    
    * minimal template
    
    * add netlify.toml
    
    * add nvmrc with stable version
    
    * add github actions for cicd
    
    * change nvm version to 18.2.0
    
    * change nvm to 17.9.0
    
    * add react and tailwind
    
    * add example blog page with markdown and add react component to md page
    
    * remove tailwind
    
    * setup layouts and add css reset
    
    * syntax highlighting with shiki
    
    * setup dark mode
    
    * refactor DarkModeButton to astro component
    
    * update meta viewport tag
    
    * add darkmode button
    
    * add styleguide page
    
    * add prettier-plugin-astro
    
    * add mdx
    
    * map over blog posts on blog page
    
    * update packages
    
    * light/dark mode with BlogCard list on home page
    
    * map over mdx files on home page
    
    * Add watch compile scss blog post
    
    * responsive max-width using clamp
    
    * add padding-bottom: 48px to home page
    
    * add Nav
    
    * add flex-wrap for profile name and Nav
    
    * create Link component, cleanup styles
    
    * more fixes
    
    * basic styles for /blog
    
    * styles for BlogPostLayout
    
    * table of contents and animated buttons
    
    * get headings from Astro.props
    
    * add heading interface
    
    * corn time is done. back to contents
    
    * add prettier pre-commit blog
    
    * fix line highlight syntax
    
    there isn't an easy intergration for highlighting right now in
    astro, mdx or shiki but keeping this here if it becomes easier later
    
    * fix link styles for table of contents
    
    * fix ol styles on blogpost
    
    * add styles for photo credits
    
    * fix BlogCard width styles
    
    * delete mock BlogCards from home page
    
    * delete mock blog post
    
    * add blog for how i start every project
    
    * add parcel blog post
    
    * add blog post for how to generate blog post hygen
    
    * fix padding bottom on /blog page
    
    * delete FancyButton
    
    * add git squash blog
    
    * add WSL blog
    
    * add meta
    
    * delete styleguide
    
    * add past design images for 2018
    
    * use optional chaining for pics.map
    
    * fix layout
    
    * hide /blog and /meta links in Nav
    
    * add style overrides to get Nav styles working on blog routes
    
    * hide "Read more" component
    
    * remove client:visible from Astro component
    
    * update redirects

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), mdx()],
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: "dracula",
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true
+    }
+  }
+});
--- jest-configs/jest-preprocess.js
+++
@@ -1,5 +0,0 @@
-const babelOptions = {
-  presets: ['@babel/preset-react', 'babel-preset-gatsby'],
-};
-
-module.exports = require('babel-jest').createTransformer(babelOptions);
