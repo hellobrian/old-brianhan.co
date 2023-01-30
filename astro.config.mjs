f9c7013ac4a0736c7d3f7a07e6a31abc16bd78fb
hellobrian <brianhan@hey.com>
2023-01-30 16:33:31 -0600

    add svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://brianhan.co/",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), svelte()]
 });
