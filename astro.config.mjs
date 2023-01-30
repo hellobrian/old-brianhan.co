0e1f659e1fbe17ab3d3a4ce221c903054cbf97aa
hellobrian <brianhan@hey.com>
2023-01-29 19:18:25 -0600

    update to astro v2 using blog template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,10 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
-import mdx from "@astrojs/mdx";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],
-  markdown: {
-    shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
-      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: "dracula",
-      // Add custom languages
-      // Note: Shiki has countless langs built-in, including .astro!
-      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-      langs: [],
-      // Enable word wrap to prevent horizontal scrolling
-      wrap: true
-    }
-  }
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
 });
